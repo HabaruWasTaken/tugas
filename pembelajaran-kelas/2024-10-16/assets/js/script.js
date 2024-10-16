9f94b63c42520b20292d363ffbb34cc6fe9d91c7
HabaruWasTaken <evanadhiarjayohanes@gmail.com>
2024-10-17 00:54:11 +0700

    update

---
+++ pembelajaran-kelas/2024-10-16/assets/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const elToggleFunc = function (el) { 
+    el.classList.toggle("active"); 
+}
+
+const navbar = document.querySelector("[data-nav]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElArr.length; i++) {
+    navElArr[i].addEventListener("click", function () {
+        elToggleFunc(navbar);
+        elToggleFunc(overlay);
+        elToggleFunc(document.body);
+    })
+}
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 800) {
+        goTopBtn.classList.add("active");
+    } else {
+        goTopBtn.classList.remove("active");
+    }
+})
